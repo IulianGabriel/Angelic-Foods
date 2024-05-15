0911cc68659de5cf6250250ae63cb3abafb25a6a
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-05-15 17:42:25 +0300

    +Css Changes  +MainPage content

--- gui/src/components/HomePage.jsx
+++ gui/src/components/HomePage.jsx
@@ -1,9 +1,18 @@
+import mainImage from "../assets/mainImage.jpg";
+import { IoArrowDown } from "react-icons/io5";
+import "./homepage.css";
 const HomePage = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-      <h1>Hey does this work?</h1>
-    </>
+    <div className="image-container">
+      <img className="main-image" src={mainImage} />
+      <div className="absolute-text">
+        <h1 className="restaurant-name">Angelic Food</h1>
+        <h3 className="restaurant-slogan">Enjoy the heavenly taste</h3>
+        <button className="restaurant-seeMore">
+          See Below <IoArrowDown />
+        </button>
+      </div>
+    </div>
   );
 };
 
