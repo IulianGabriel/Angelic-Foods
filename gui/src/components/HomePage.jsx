813450bbec36282bc9bead75cecc5a9d3418aba8
Iulian Gabriel Burciu <iuliangabrielbig@gmail.com>
2024-05-16 17:08:46 +0300

    +food carousel +css +structure

--- gui/src/components/HomePage.jsx
+++ gui/src/components/HomePage.jsx
@@ -1,7 +1,11 @@
 import mainImage from "../assets/mainImage.jpg";
-import mealImage from "../assets/MealPhoto.jpg";
+import { carouselFoods } from "../utils/carouselFoods";
 import { IoArrowDown } from "react-icons/io5";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Link } from "react-router-dom";
 import "./homepage.css";
+
 const HomePage = () => {
   return (
     <>
@@ -18,16 +22,67 @@ const HomePage = () => {
         </div>
       </div>
       <div className="meals-container" id="mealImage">
-        <h1 className="promote-meals-text">Check out our freshest meals!</h1>
-        {/* <div className="mealImage-container">
-          <img className="mealImage" src={mealImage} alt="Meal Image" />
-          <img className="mealImage" src={mealImage} alt="Meal Image" />
-          <img className="mealImage" src={mealImage} alt="Meal Image" />
-          <img className="mealImage" src={mealImage} alt="Meal Image" />
-          <img className="mealImage" src={mealImage} alt="Meal Image" />
-          <img className="mealImage" src={mealImage} alt="Meal Image" />
-        </div> */}
+        <h1 className="promote-meals-text">View our selection!</h1>
+        <hr />
+        <div className="foodType-container">
+          <Carousel
+            autoPlay={true}
+            additionalTransfrom={0}
+            arrows
+            autoPlaySpeed={2500}
+            centerMode={false}
+            className=""
+            containerClass="container-with-dots"
+            draggable={false}
+            focusOnSelect={false}
+            infinite
+            keyBoardControl
+            pauseOnHover
+            renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+              desktop: {
+                breakpoint: { max: 3000, min: 1024 },
+                items: 3,
+              },
+              tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 2,
+              },
+              mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 1,
+              },
+            }}
+            rewind={false}
+            rewindWithAnimation={false}
+            rtl={false}
+            shouldResetAutoplay
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={1}
+            swipeable
+          >
+            {carouselFoods.map((food, index) => (
+              <div key={index}>
+                <div className="carousel-item">
+                  <img className="foodtype-image" src={food.img} />
+                  <p className="foodtype-description">{food.description}</p>
+                  <Link
+                    style={{ marginBottom: "2rem" }}
+                    to={food.type.toLowerCase()}
+                    className="foodtype-button"
+                  >
+                    {food.type}
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </div>
+      Z
     </>
   );
 };
---
+++ gui/src/utils/carouselFoods.js
@@ -0,0 +1,52 @@
+import mealImage from "../assets/MealPhoto.jpg";
+import pizzaImage from "../assets/PizzaImage.jpg";
+import burgerImage from "../assets/BurgerImage.jpg";
+import pastaImage from "../assets/PastaImage.jpg";
+import dessertImage from "../assets/DessertImage.jpg";
+import beverageImage from "../assets/BeverageImage.jpg";
+import sidesImage from "../assets/SidesImage.jpg";
+
+export const carouselFoods = [
+  {
+    type: "Meals",
+    description:
+      "Experience the pinnacle of freshness with our mouthwatering meals!",
+    img: mealImage,
+  },
+  {
+    type: "Pizza",
+    description:
+      "Indulge in the rustic charm of our stone oven-baked pizzas, crafted with care for that perfect crispy crust and irresistible toppings.",
+    img: pizzaImage,
+  },
+  {
+    type: "Burgers",
+    description:
+      "Dive into our mouthwatering, juicy burgers for a taste sensation you won't forget",
+    img: burgerImage,
+  },
+  {
+    type: "Pasta",
+    description:
+      "Twirl your fork into a world of flavor with our delectable pasta dishes, crafted with the finest ingredients and simmered to perfection.",
+    img: pastaImage,
+  },
+  {
+    type: "Desserts",
+    description:
+      "Treat yourself to our tempting desserts, crafted with care to satisfy every craving",
+    img: dessertImage,
+  },
+  {
+    type: "Beverages",
+    description:
+      "Quench your thirst with our refreshing beverages, crafted with quality ingredients to complement any meal or occasion.",
+    img: beverageImage,
+  },
+  {
+    type: "Sides",
+    description:
+      "Pair your meal with our tasty sides, from crispy fries to flavorful sauces, juicy wings and more for the perfect accompaniment to your dish.",
+    img: sidesImage,
+  },
+];
